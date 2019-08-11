8bab0ace5b42e3d865b8a2f365d9c65f3d8096f6
Paul Rosero <paul_rosero@yahoo.com>
2019-08-11 17:41:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
-  get '/newteam' do 
+  
+  get '/newteam' do
     erb :newteam
-  en
+  end
   
   post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
     erb :team
   end
 
